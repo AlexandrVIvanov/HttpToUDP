a542b3a5c1a65bbd00379220cc9d60011ea2b81a
dianddi <dianddi@mail.com>
2022-12-06 23:46:19 +0400

    Долго мучался между двумя средами

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/http"
@@ -18,13 +19,29 @@ func main() {
 func echoPayload(w http.ResponseWriter, req *http.Request) {
 
 	log.Printf("Request connection: %s, path: %s", req.Proto, req.URL.Path[1:])
-	defer req.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("error: %s", err)
+		}
+	}(req.Body)
 
 	q := req.URL.Query()
 
+	_, err2 := fmt.Fprintln(w, "Helo Andru")
+	if err2 != nil {
+		return
+	}
+
 	if (!q.Has("ip")) || (!q.Has("text")) {
-		fmt.Fprintf(w, "Query string dont have ?ip=... param %s\n", string(req.URL.RawPath))
-		//fmt.Fprintln(w, "Request connections http://urlservice:9191/hello/sendudp?ip=ipadress(hexdecimal format)&text=text(hexdecimalformat)")
+		_, err3 := fmt.Fprintf(w, "Query string dont have ?ip=... param %s\n", string(req.URL.RawPath))
+		if err3 != nil {
+			return
+		}
+		_, err := fmt.Fprintln(w, "Request connections http://urlservice:9191/hello/sendudp?ip=ipadress(hexdecimal format)&text=text(hexdecimalformat)")
+		if err != nil {
+			return
+		}
 		return
 	}
 
@@ -47,12 +64,14 @@ func echoPayload(w http.ResponseWriter, req *http.Request) {
 	log.Printf("Send: %s, path: %s", string(qiphex), string(qtexthex))
 	log.Printf("Send: %s, path: %s", string(qtext), string(qip))
 
-
 	go sendToUDP(qip, qtext)
 
 	//	fmt.Fprintf(w, "%s\n", string(contents))
 	//	fmt.Fprintf(w, "%s\n", string(qip))
-		fmt.Fprintf(w, "%s\n", string(qtext))
+	_, err2 = fmt.Fprintf(w, "%s\n", string(qtext))
+	if err2 != nil {
+		return
+	}
 
 }
 
@@ -71,5 +90,8 @@ func sendToUDP(ip []byte, text []byte) {
 		return
 	}
 
-	conn.Close()
+	err = conn.Close()
+	if err != nil {
+		return
+	}
 }
