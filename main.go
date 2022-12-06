c9bab9ac2019e73bf832ff181d52282f5bd3f780
Alexandr <slimmaster1974@gmail.com>
2022-12-06 14:39:58 +0400

    Add files via upload

--- main.go
+++ main.go
@@ -1,37 +1,73 @@
 package main
 
 import (
-	fmt "fmt"
-	"io"
+	"encoding/hex"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
+
 	http.HandleFunc("/hello/sendudp", echoPayload)
-	log.Printf("Go Backend: { HTTPVersion = 1 }; serving on https://localhost:9191/hello/sendudp")
+	log.Printf("Go Backend: { HTTPVersion = 1 }; serving on http://localhost:9191/hello/sendudp")
 	log.Fatal(http.ListenAndServe(":9191", nil))
 }
 
 func echoPayload(w http.ResponseWriter, req *http.Request) {
-	log.Printf("Request connection: %s, path: %s", req.Proto, req.URL.Path[1:])
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			log.Fatalf(
-				"Oops! Failed close connection.\n %s",
-				err,
-			)
-		}
-	}(req.Body)
 
-	var contents, err = io.ReadAll(req.Body)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		log.Fatalf("Oops! Failed reading body of the request.\n %s", err)
-	}
-	_, err = fmt.Fprintf(w, "%s\n", string(contents))
-	if err != nil {
+	log.Printf("Request connection: %s, path: %s", req.Proto, req.URL.Path[1:])
+	defer req.Body.Close()
+
+	q := req.URL.Query()
+
+	if (!q.Has("ip")) || (!q.Has("text")) {
+		fmt.Fprintf(w, "Query string dont have ?ip=... param %s\n", string(req.URL.RawPath))
+		//fmt.Fprintln(w, "Request connections http://urlservice:9191/hello/sendudp?ip=ipadress(hexdecimal format)&text=text(hexdecimalformat)")
 		return
 	}
+
+	qiphex := q.Get("ip")
+	qtexthex := q.Get("text")
+
+	qip, err := hex.DecodeString(qiphex)
+	if err != nil {
+		log.Printf("error: %s", err)
+		log.Printf("Send qip: %s", string(qiphex))
+		return
+	}
+
+	qtext, err := hex.DecodeString(qtexthex)
+	if err != nil {
+		log.Printf("error: %s", err)
+		log.Printf("Send qtext: %s", string(qtexthex))
+		return
+	}
+	log.Printf("Send: %s, path: %s", string(qtext), string(qip))
+
+	go sendToUDP(qip, qtext)
+
+	//	fmt.Fprintf(w, "%s\n", string(contents))
+	//	fmt.Fprintf(w, "%s\n", string(qip))
+	//	fmt.Fprintf(w, "%s\n", string(qtext))
+
+}
+
+func sendToUDP(ip []byte, text []byte) {
+	conn, err := net.Dial("udp", string(ip))
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		log.Printf("Some error %v", err)
+		return
+	}
+	//	sbyte := []byte(s)
+	_, err = conn.Write(text)
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		log.Printf("Some error %v", err)
+		return
+	}
+
+	conn.Close()
 }
